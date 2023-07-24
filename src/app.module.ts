c0d1c5c158ef0e6f15b90df199822a7bd4ea06be
Dr.Phen <Vlimaster@yandex.ru>
2023-07-24 17:22:43 +0700

    fixed .gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { NotesModule } from './notes/notes.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+
 @Module({
   imports: [NotesModule, AuthModule, UsersModule],
   controllers: [AppController],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,21 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '@/users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
   async signIn(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findUser(username);
     if (user?.password !== pass) {
       throw new UnauthorizedException();
     }
-    const { password, ...result } = user;
+    // const { password, ...result } = user;
+    const payload = { sub: user.id, username: user.username };
     // TODO: Generate a JWT and return it here
-    return result;
+    return { access_token: await this.jwtService.signAsync(payload) };
   }
 }
