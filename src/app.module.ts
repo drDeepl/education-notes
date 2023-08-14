ee93306594e1cf74c8af47068b94af87cafdc3bd
Dr.Phen <Vlimaster@yandex.ru>
2023-08-14 17:26:56 +0700

    create image module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { ImageModule } from './image/image.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { PrismaModule } from './prisma/prisma.module';
       cache: true,
     }),
     PrismaModule,
+    ImageModule,
   ],
   controllers: [AppController],
   providers: [AppService],
